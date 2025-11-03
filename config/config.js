8c6fdbfda3bc1d9cf9fe41b4315edf6d54b59f4c
lilalr <gurennut@gmail.com>
2025-11-03 11:27:30 +0700

    first commit

---
+++ config/config.js
@@ -0,0 +1,32 @@
+// setup express
+
+require("dotenv").config();
+
+const development = {   // pastikan nama DB" nya sama dengan yg di dalam file .env
+  username: process.env.DB_USER,  
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT
+}
+
+const test = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE_TEST,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT
+};
+
+const production = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE_PROD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT
+};
+
+module.exports = { development, test, production };
